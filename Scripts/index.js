0865b6fc607dbf9e92d47d3419eec48b65f9dcf7
Dino_Dev <makstinyworkshop@gmail.com>
2024-02-05 07:50:56 +0100

    Refactorisation

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,15 +2,17 @@
 Récupération des infos du JSON
 ----------------------------*/
 function recupDonnees() {
-  fetch("/assets/promo.json")
-    .then((response) => {
+  Promise.all([
+    fetch("/assets/promo.json").then((response) => {
       return response.json();
-    })
-    .then((data) => {
-      fillElements(data);
-      handleLocalStorage(data);
-    })
-    .then(listenModal);
+    }),
+    fetch("/assets/variables.json").then((response) => {
+      return response.json();
+    }),
+  ]).then((data) => {
+    fillElements(data);
+    handleLocalStorage(data);
+  });
 }
 recupDonnees();
 
@@ -21,47 +23,39 @@ Gestion de l'affichage en fonction du check Radio
 const displayList = document.getElementById("liste");
 const displayCard = document.getElementById("cartes");
 
-// Elements correspondants (tableau ou div de cards ou modals)
+// Pointage du tableau et de son template
 const studentTable = document.getElementById("studentTable");
-const studentCards = document.getElementById("studentCards");
-const studentModals = document.getElementById("studentModals");
 
-// Par défaut, masquer le mode Card index.html
-if (studentCards) {
-  studentCards.style.display = "none";
-}
+// Par défaut, disposition tableau
+studentTable.classList.add("opened");
 
-// EventListeners sur les radio
-displayCard.addEventListener("change", toggleDisplay);
+// EventListeners sur le radio liste (tableau)
 displayList.addEventListener("change", toggleDisplay);
 
+// Fonction d'affichage Tableau / Cards
 function toggleDisplay() {
   if (liste.checked) {
-    studentTable.style.display = "table";
-    studentCards.style.display = "none";
+    studentTable.classList.add("opened");
+    studentCards.classList.remove("opened");
   } else if (cartes.checked) {
-    studentTable.style.display = "none";
-    studentCards.style.display = "grid";
+    studentTable.classList.remove("opened");
+    studentCards.classList.add("opened");
   }
 }
 
-/*----------------------------------------------
-Récupération des données de la DB et remplissage
-----------------------------------------------*/
+/*------------------------------------------------------
+Récupération des données de la DB et remplissage tableau
+------------------------------------------------------*/
 const templateArray = document.getElementById("templateStudentArray");
-const templateCard = document.getElementById("templateStudentCard");
-const templateModal = document.getElementById("templateStudentModal");
 
 const studentArray = document.querySelector("tbody");
-
-// Fonction de remplissage dynamique des cards et lignes de tableau en fonction du fichier promo.json
+// Fonction de remplissage dynamique lignes de tableau en fonction du fichier promo.json
 function fillElements(data) {
-  data = data.apprenants;
+  data = data[0].apprenants;
 
+  displayStudentCards(data);
   for (let i = 0; i < data.length; i++) {
     const clone = templateArray.content.cloneNode(true);
-    const clone2 = templateCard.content.cloneNode(true);
-    const clone3 = templateModal.content.cloneNode(true);
 
     // Remplissage tableau
     let td = clone.querySelectorAll("td");
@@ -71,30 +65,9 @@ function fillElements(data) {
     // Attribution d'un nombre = id pour cibler plus facilement
     td[3].setAttribute("data-number", data[i].id);
 
-    // Remplissage cards
-    let p = clone2.querySelectorAll("p");
-    p[0].textContent = data[i].nom;
-    p[1].textContent = data[i].prenom;
-    p[2].textContent = data[i].ville;
-    // Attribution d'un nombre = id pour cibler plus facilement
-    p[3].setAttribute("data-number", data[i].id);
-
-    // Remplissage modals
-    let p2 = clone3.querySelectorAll("p");
-    let c = clone3.querySelector(".modal");
-    let img = clone3.querySelector("img[type='avatar']");
-    c.setAttribute("data-number", data[i].id);
-    img.setAttribute("src", data[i].avatar);
-    img.setAttribute("alt", `Avatar de ${data[i].prenom} ${data[i].nom}`);
-    p2[0].innerHTML = "<strong>Nom : </strong>" + data[i].nom;
-    p2[1].innerHTML = "<strong>Prénom : </strong>" + data[i].prenom;
-    p2[2].innerHTML = "<strong>Ville : </strong>" + data[i].ville;
-    p2[3].textContent = data[i].anecdotes;
-
     // Ajout dans leurs div respectives
     studentArray.appendChild(clone);
-    studentCards.appendChild(clone2);
-    studentModals.appendChild(clone3);
+    listenModal();
   }
 }
 
@@ -104,7 +77,8 @@ En fonction du localStorage, gestion couleurs et affichage et remplissage dynami
 function handleLocalStorage(data) {
   // Récupération des données localStorage
   let prefs = JSON.parse(localStorage.getItem("Préférences"));
-  let infos = data.infoPromo;
+  let infos = data[0].infoPromo;
+  let couleurFont = data[1].clair.Blanc;
 
   // Remplissage du Titre
   const promo = document.getElementById("promo");
@@ -117,7 +91,7 @@ function handleLocalStorage(data) {
       document.body.style.backgroundColor = prefs.backgroundColor;
     } else {
       document.body.style.backgroundColor = prefs.backgroundColor;
-      document.body.style.color = "#fff";
+      document.body.style.color = couleurFont;
     }
     // Gestion du bouton radio
     if (prefs.display == "liste") {
@@ -130,54 +104,114 @@ function handleLocalStorage(data) {
   }
 }
 
+/*-------------------------------
+Fonction de remplissage des cards
+-------------------------------*/
+// Pointage de la div de cards et template correspondant
+const studentCards = document.getElementById("studentCards");
+const templateCard = document.getElementById("templateStudentCard");
+
+// Fonction de remplissage et affichage
+function displayStudentCards(data) {
+  // Event Listener sur le bouton radio
+  displayCard.addEventListener("change", () => {
+    // Si check et div card vide
+    if (displayCard.checked && studentCards.children.length == 0) {
+      for (let i = 0; i < data.length; i++) {
+        const clone2 = templateCard.content.cloneNode(true);
+
+        // Remplissage cards
+        let p = clone2.querySelectorAll("p");
+        p[0].textContent = data[i].nom;
+        p[1].textContent = data[i].prenom;
+        p[2].textContent = data[i].ville;
+        // Attribution d'un nombre = id pour cibler plus facilement
+        p[3].setAttribute("data-number", data[i].id);
+
+        // Ajout dans la div
+        studentCards.appendChild(clone2);
+        toggleDisplay();
+        listenModal();
+      }
+      // Sinon fonction toggle
+    } else if (displayCard.checked) {
+      toggleDisplay();
+      listenModal();
+    }
+  });
+}
+
 /*------------------
 Gestion de la modale
 ------------------*/
+// Pointage de la div de modale et de son template
+const studentModals = document.getElementById("studentModals");
+const templateModal = document.getElementById("templateStudentModal");
+
 // Fonction appelée après le fetch pour gérer l'arrivée des données
 function listenModal() {
   const details = document.getElementsByClassName("details");
   for (let i = 0; i < details.length; i++) {
-    details[i].addEventListener("click", displayModal);
+    //details[i].addEventListener("click", displayModal);
+    details[i].addEventListener("click", displayStudentModal);
   }
 }
 
-// Fonction d'affichage d'une modale au clic pour chaque étudiant
-function displayModal(e) {
-  // Récupération de l'attribut data-number pour display modale correspondante
-  const dataNumber = e.target.getAttribute("data-number");
-  const modalToDisplay = document.querySelector(
-    ".modal[data-number='" + dataNumber + "']"
-  );
-  modalToDisplay.classList.add("opened");
-  e.stopPropagation();
-  // Fonction Fun qui position la modale aléatoirement
-  const maxWidth = window.innerWidth - modalToDisplay.offsetWidth;
-  const maxHeight = window.innerHeight - modalToDisplay.offsetHeight;
+function displayStudentModal(e) {
+  fetch("/assets/promo.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const donnees = data.apprenants;
+      const dataNumber = e.target.getAttribute("data-number") - 1;
+      const clone = templateModal.content.cloneNode(true);
 
-  // Calculer une position aléatoire sans dépasser les limites de l'écran
-  const randomLeft = Math.floor(Math.random() * maxWidth);
-  const randomTop = Math.floor(Math.random() * maxHeight);
+      // Remplissage modal
+      let p2 = clone.querySelectorAll("p");
+      let c = clone.querySelector(".modal");
+      c.setAttribute("data-number", donnees[dataNumber].id);
+      let img = clone.querySelector("img[type='avatar']");
+      img.setAttribute("src", donnees[dataNumber].avatar);
+      img.setAttribute(
+        "alt",
+        `Avatar de ${donnees[dataNumber].prenom} ${donnees[dataNumber].nom}`
+      );
+      p2[0].innerHTML = "<strong>Nom : </strong>" + donnees[dataNumber].nom;
+      p2[1].innerHTML =
+        "<strong>Prénom : </strong>" + donnees[dataNumber].prenom;
+      p2[2].innerHTML = "<strong>Ville : </strong>" + donnees[dataNumber].ville;
+      p2[3].textContent = donnees[dataNumber].anecdotes;
 
-  // Seulement sur écran > tablette
-  if (window.innerWidth >= 768) {
-    modalToDisplay.style.left = `${randomLeft}px`;
-    modalToDisplay.style.top = `${randomTop}px`;
-  } else {
-    modalToDisplay.style.right = `calc(50% + 1rem)`;
-    modalToDisplay.style.bottom = `40%`;
-  }
+      if (
+        !studentModals.querySelector(
+          `[data-number="${donnees[dataNumber].id}"]`
+        )
+      ) {
+        studentModals.appendChild(clone);
+        c.classList.add("opened");
+        // Fonction Fun qui position la modale aléatoirement
+        const maxWidth = window.innerWidth - c.offsetWidth;
+        const maxHeight = window.innerHeight - c.offsetHeight;
 
-  // EventListener pour fermer la modale au clic sur X
-  const close = modalToDisplay.querySelector("span[type='close']");
-  close.addEventListener("click", () => {
-    modalToDisplay.classList.remove("opened");
-  });
+        // Calculer une position aléatoire sans dépasser les limites de l'écran
+        const randomLeft = Math.floor(Math.random() * maxWidth);
+        const randomTop = Math.floor(Math.random() * maxHeight);
+
+        // Seulement sur écran > tablette
+        if (window.innerWidth >= 768) {
+          c.style.left = `${randomLeft}px`;
+          c.style.top = `${randomTop}px`;
+        } else {
+          c.style.right = `calc(50% + 1rem)`;
+          c.style.bottom = `40%`;
+        }
+      }
+    });
 }
 
 //Fonction qui permet de fermer une modale au clic en dehors de celle-ci
 document.addEventListener("click", function () {
-  const openedModal = document.querySelector(".opened");
+  const openedModal = document.querySelector(".modal.opened");
   if (openedModal) {
-    openedModal.classList.remove("opened");
+    studentModals.removeChild(openedModal);
   }
 });
